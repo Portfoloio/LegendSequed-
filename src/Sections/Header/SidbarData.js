7d90a0b76d43c3a1be6d82b37e89ac022b2e830d
eman ebrahim <adla48770@gmail.com>
2021-02-12 19:39:42 +0200

    Navbar

---
+++ src/Sections/Header/SidbarData.js
@@ -0,0 +1,44 @@
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+
+    cName: "nav-text",
+  },
+  {
+    title: "Services",
+    path: "/Services",
+
+    cName: "nav-text",
+  },
+  {
+    title: "Portfolio",
+    path: "/Portfolio",
+    cName: "nav-text",
+  },
+  {
+    title: "Experience",
+    path: "/Experience",
+    cName: "nav-text",
+  },
+  {
+    title: "Skills",
+    path: "/Skills",
+    cName: "nav-text",
+  },
+  {
+    title: "Testimonials",
+    path: "/Testimonials",
+    cName: "nav-text",
+  },
+  {
+    title: "News",
+    path: "/News",
+    cName: "nav-text",
+  },
+  {
+    title: "Contact",
+    path: "/Contact",
+    cName: "nav-text",
+  },
+];
