825dbe949c2dd57adf223b57c29cface996cb912
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-02-26 15:49:49 +0200

    no need

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import Navbar from "./Sections/Header/Navbar";
-import useSticky from "./Hooks/useSticky";
 import Welcome from "./Sections/Header/Welcome";
 import Portfolio from "./Sections/Portfolio/index";
 import Testimonial from "./Sections/Testimonial/index";
 
 function App() {
-  const { isSticky, element } = useSticky();
   return (
     <div className="App">
-      <Navbar sticky={isSticky} />
-      <Welcome element={element} />
+      <Navbar />
+      <Welcome />
       <Portfolio />
       <Testimonial />
     </div>
