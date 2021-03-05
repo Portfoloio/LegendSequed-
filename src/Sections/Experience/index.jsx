e028883d975d3bc748df6a54b28cb8c5cb524145
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-03-05 12:25:29 +0200

    setup

---
+++ src/Sections/Experience/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as E from "./style";
+
+export default function Experience() {
+  return (
+    <E.Section>
+      <h1>sss</h1>
+    </E.Section>
+  );
+}
---
+++ src/Sections/Experience/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+  background-color: ${({ theme }) => theme.sectionBgC};
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
