99ab3d9578dbb181a73df50974dfd7939102e63e
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-03-18 13:17:30 +0200

    set up files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Experience from "./Sections/Experience";
 import Navbar from "./Sections/Header/Navbar";
 import Welcome from "./Sections/Header/Welcome";
 import Portfolio from "./Sections/Portfolio/index";
+import Skills from "./Sections/Skills";
 import Testimonial from "./Sections/Testimonial/index";
 import Services from "./Sections/WhatWeDo/index";
 
@@ -64,6 +65,7 @@ function App() {
         <Services />
         <Portfolio theme={theme} />
         <Experience />
+        <Skills />
         <Testimonial />
       </ThemeProvider>
     </main>
